cd7eff5147f39f9d3d50ed60f1a1130279853df6
Pavan Khandelwal <pavankhandelwal1305@gmail.com>
2026-04-22 17:57:48 +0530

    Database  updated

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   mongoose.connection.on("connected", () => console.log("Database Connected"));
-  await mongoose.connect(`${process.env.MONGODB_URI}/image-generator`);
+  await mongoose.connect(`${process.env.MONGODB_URI}`);
 };
 
 export default connectDB;
